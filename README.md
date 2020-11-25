146776c01375e15329c1fd3d863a496b832f7eda
Kirill <kiruxastar@gmail.com>
2020-11-25 19:50:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/md31c1ah8x62jo1p?svg=true)](https://ci.appveyor.com/project/kirmakin/postmanecho)
---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
+public class PostmanEchoTest {
+    String s = "some data";
+
+    @Test
+    public void shouldReturnStandardObject() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(s) // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo(s))
+                .body("headers.content-length", equalTo("9"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.8)"))
+                .body(matchesJsonSchemaInClasspath("postmanEcho.schema.json"))
+        ;
+    }
+}
